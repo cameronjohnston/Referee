a95db27bf80279c0c13bbde17c63a0f2d947312d
Cameron Johnston <cameron.johnston8@gmail.com>
2015-10-11 08:27:14 -0700

    Initial Commit

---
+++ Referee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Referee
+//
+//  Created by Cameron Johnston on 2015-10-11.
+//  Copyright © 2015 Cameruleman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
